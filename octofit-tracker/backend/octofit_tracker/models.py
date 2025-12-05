0cf0d4fc7f3a6c7df6e317ead357ff3147406dc8
Sigma <mukae.yuta.0811@gmail.com>
2025-12-05 22:29:50 +0900

    Djangoプロジェクトの初期設定とモデル、管理コマンドを追加

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,46 +1,69 @@
+
+
 from djongo import models
+from bson import ObjectId
+
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False, default=lambda: ObjectId())
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
+
     class Meta:
         db_table = 'teams'
+
     def __str__(self):
         return self.name
 
+
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False, default=lambda: ObjectId())
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='members')
     is_superhero = models.BooleanField(default=False)
+
     class Meta:
         db_table = 'users'
+
     def __str__(self):
         return self.name
 
+
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False, default=lambda: ObjectId())
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     type = models.CharField(max_length=100)
     duration = models.PositiveIntegerField()
     date = models.DateField()
+
     class Meta:
         db_table = 'activities'
+
     def __str__(self):
         return f"{self.user.name} - {self.type}"
 
+
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False, default=lambda: ObjectId())
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     suggested_for = models.ManyToManyField(User, blank=True, related_name='suggested_workouts')
+
     class Meta:
         db_table = 'workouts'
+
     def __str__(self):
         return self.name
 
+
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False, default=lambda: ObjectId())
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='leaderboard_entries')
     score = models.IntegerField()
+
     class Meta:
         db_table = 'leaderboard'
+
     def __str__(self):
         return f"{self.user.name} - {self.score}"
